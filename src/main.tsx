78da366a1ee095ce5fd68ee3f56de03608b1b2f9
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2025-09-29 15:58:38 +0700

    recover usage index.css on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {RouterProvider} from "react-router";
 import router from "./app/router";
-// import "./styles/global.css"; // Uncomment if you have global styles
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
